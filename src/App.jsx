16b300f4b33bf755c1f73b081e4269c3660de06c
ArnabShikder24 <arnabshikder2001@gmail.com>
2023-06-07 20:48:08 +0600

    3rd reg done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Constraction from './pages/Constraction/Constraction';
 import Registration from './pages/Registration/Registration';
 import Registration1 from './pages/Registration/Registration1';
+import Registration2 from './pages/Registration/Registration2';
 import pathname from './routes';
 import { Route, Routes } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
         <Route path={pathname.home} element={<Constraction />} ></Route>
         <Route path={pathname.registration} element={<Registration />} ></Route>
         <Route path={pathname.registration1} element={<Registration1 />} ></Route>
+        <Route path={pathname.registration2} element={<Registration2 />} ></Route>
       </Routes>
     </div>
   );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 const pathname = {
     home: '/',
     registration: '/registration',
-    registration1: '/registration1'
+    registration1: '/registration1',
+    registration2: '/registration2'
 }
  
 export default pathname;
